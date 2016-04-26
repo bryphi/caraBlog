30a57b809e9409f504a5a00c903ac8958561fd6b
caraBlog <brycejphillips@gmail.com>
2016-04-26 21:10:25 +0000

    added new page, article routes, and updated article controller

--- config/routes.rb
+++ config/routes.rb
@@ -12,10 +12,13 @@ Rails.application.routes.draw do
    get '/contact' => 'pages#contact'
    get '/portfolio' => 'pages#portfolio'
    get '/resume' => 'pages#resume'
-   get '/blog' => 'blog#blogHome'
-   get '/new' => 'blog#new'
-   get '/blog/:id' => 'blog#show'
-   get '/blog/:id/edit' => 'blog#edit'
+   
+   resources :articles
+   
+   #get '/blog' => 'blog#blogHome'
+   #get '/new' => 'blog#new'
+   #get '/blog/:id' => 'blog#show'
+   #get '/blog/:id/edit' => 'blog#edit'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
